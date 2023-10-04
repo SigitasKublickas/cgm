e43fc4b01b5cc22108eee49e09c8b087330383ac
Sigitas <sig.kublickas99@gmail.com>
2023-10-05 00:35:17 +0300

    navbar and sidebar lists and types

---
+++ src/helpers/lists.ts
@@ -0,0 +1,12 @@
+import { NavBarListType, SideBarListType } from "./types";
+
+export const navbarList: NavBarListType = [
+  { name: "Pagrindinis", href: "/" },
+  { name: "Apie mus", href: "/about" },
+  { name: "Kontaktai", href: "/contact" },
+];
+export const sidebarList: SideBarListType = [
+  { name: "reservation", href: "/reservation", img: "" },
+  { name: "cars", href: "/cars", img: "" },
+  { name: "schedule", href: "/schedule", img: "" },
+];
---
+++ src/helpers/types.ts
@@ -0,0 +1,5 @@
+export enum LogIn {}
+
+// Lists types
+export type NavBarListType = { name: string; href: string }[];
+export type SideBarListType = { name: string; href: string; img: string }[];
